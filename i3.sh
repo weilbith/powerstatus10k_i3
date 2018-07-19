8e711b2861fd8c9da4d22f91b9f49cdfb2500f2d
weilbith <thore@weilbier.net>
2018-07-19 22:40:42 +0200

    Initial commit with the whole base functionality.

---
+++ i3.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+# PowerStatus10k segment.
+
+function initState_i3 {
+  # Start the subscribing python script.
+  $(dirname ${BASH_SOURCE[0]})/i3subscriber.py &
+  PID_LIST="$PID_LIST $!"
+  STATE="1 ${I3_SEPARATOR_CURRENT}"
+}
+
+function format_i3 {
+  window="${1%:*}"
+  window="${window##*-} - ${window%%-*}"
+  windowAbbr=$(abbreviate "$window")
+
+  workspaceString="${1##*:}"
+  IFS=',' read -ra workspaces <<< "$workspaceString"
+
+  formatString=""
+  separator="$I3_SEPARATOR_LEFT" # Will be switched after the current workspace.
+  length=${#workspaces[@]}
+  last=$((length - 1))
+
+  for (( i=0; i<$length; i++)) ; do
+    workspace=${workspaces[i]}
+
+    # Check if this is the current workspace by the signal char.
+    if [[ "$workspace" =~ "!" ]] ; then
+      formatString="${formatString} ${workspace:1} ${I3_SEPARATOR_CURRENT} ${windowAbbr}" # Do not forget to cut of the leading singal char.
+      separator="$I3_SEPARATOR_RIGHT" # Switch the separator here.
+
+    else
+      formatString="${formatString} ${workspace}"
+    fi
+
+    # Add the current separator if this is not the last workspace.
+    if [[ $i -lt $last ]] ; then
+      formatString="${formatString} %{T2}${separator}%{T1}"
+    fi
+  done
+
+  STATE="$formatString"
+}
---
+++ i3subscriber.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Imports
+import os
+import functools
+import i3ipc
+
+# Properties
+fifoName = '/tmp/powerstatus_segment_i3'
+window = ''
+workspaces = ''
+
+# Establish connection to i3 IPC.
+i3 = i3ipc.Connection()
+
+# Pipe the current state to the FIFO.
+def pipe():
+    # Create the FIFO if not exist yet.
+    # Do it here, to avoid problems on a deleted FIFO during runtime.
+    if not os.path.exists(fifoName):
+        os.mkfifo(fifoName)
+
+    # Write to the FIFO.
+    with open(fifoName, 'w') as fifo:
+        fifo.write(window + ':' + workspaces + '\n')
+
+# Called on workspace focus event.
+def on_workspace_focus(self, e):
+    # Clear current known state of the workspaces.
+    global workspaces
+    workspaces = ''
+
+    # Concatenate the names of all workspaces.
+    for workspace in self.get_workspaces():
+        # Check if this workspace is the focused one and mark it if so.
+        if workspace['focused']:
+            workspaces +='!'
+
+        workspaces += workspace['name'] + ','
+
+    pipe()
+
+# Called on window focus event.
+def on_window_focus(self, e):
+    focused = i3.get_tree().find_focused()
+    global window
+    window = focused.name
+    pipe()
+
+# Get initial state.
+on_workspace_focus(i3, '')
+on_window_focus(i3, '')
+
+# Subscribe to event.
+i3.on('workspace::focus', on_workspace_focus)
+i3.on('window::focus', on_window_focus)
+
+# Start the main loop to get events.
+i3.main()
